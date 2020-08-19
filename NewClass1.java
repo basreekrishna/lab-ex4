29557dd5ef4d784f05bd5cc264cbf879fd3a77f0
basreekrishna <68919007+basreekrishna@users.noreply.github.com>
2020-08-19 21:37:36 +0530

    Add files via upload

---
+++ NewClass1.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sksba
+ */
+package newpackage;
+public class NewClass1 
+{        
+static
+{
+system.out.println("Welcome to Oops!!");    
+}
+
+}
+second package program 
+
+package newpackage1;
+public class NC
+{
+    static
+    {
+ system.out.println("package program!!!");
+    }
+    
+}
+accessing program
+
+package hello;
+import newpackage.*;
+import newpackage 1.*;
+public class hello
+{
+    public static void main(string[]args)
+    {
+Newclassob= new Newclass();
+    Nc ob1=new NC();
+    }
+}    
+    }       
+}
+    
+
+        
